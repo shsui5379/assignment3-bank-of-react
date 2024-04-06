364d4809b555c27bfd3db3d2ef7beebdbbc1b490
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-06 02:44:47 -0400

    pass addCredit() to Credits

--- src/App.js
+++ src/App.js
@@ -35,6 +35,10 @@ class App extends Component {
     this.setState({ currentUser: newUser })
   }
 
+  addCredit(data) {
+
+  }
+
   // Create Routes and React elements to be rendered using React components
   render() {
     // Create React elements and pass input props to components
@@ -43,7 +47,7 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} balance={this.state.accountBalance} />)
+    const CreditsComponent = () => (<Credits credits={this.state.creditList} balance={this.state.accountBalance} addCredit={this.addCredit} />)
     const DebitsComponent = () => (<Debits debits={this.state.debitList} balance={this.state.accountBalance} />)
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
