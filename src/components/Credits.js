03ed633d475541c22fad1c3f918c97f557f7b19e
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2024-04-06 22:31:24 -0400

    formatting adjustment to credits list

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -13,7 +13,7 @@ const Credits = (props) => {
     const { credits } = props;
     return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each credits JSON array element
       let date = credit.date.slice(0, 10);
-      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+      return <li key={credit.id}> ${credit.amount.toFixed(2)} -- {credit.description} -- {date}</li>
     });
   }
 
